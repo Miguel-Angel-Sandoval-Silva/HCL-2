41dbd9158aa7251b49805b4125416e2d7d4b3417
Miguel-Angel-Sandova-Silva <2230136@upv.edu.mx>
2025-12-01 17:07:26 +0000

    Fix pausa automática, overlay y reanudar PDF

--- sistemagccl/settings.py
+++ sistemagccl/settings.py
@@ -129,6 +129,10 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
